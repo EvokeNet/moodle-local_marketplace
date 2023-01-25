833dddbd1382c260a7e656454176c08fdb12c846
John Moravec <51122406+educationfutures@users.noreply.github.com>
2023-01-24 19:03:21 -0600

    Spanish lang file

---
+++ lang/es/local_marketplace.php
@@ -0,0 +1,78 @@
+<?php
+
+/**
+ * Lang file.
+ *
+ * @package     local_evokegame
+ * @copyright   2021 World Bank Group <https://worldbank.org>
+ * @author      Willian Mano <willianmanoaraujo@gmail.com>
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Evoke Marketplace';
+$string['previous'] = 'Previous';
+$string['next'] = 'Next';
+
+$string['marketplace'] = 'Marketplace';
+$string['home'] = 'Home';
+
+// Form elements.
+$string['selectanoption'] = 'Select an option';
+$string['name'] = 'Name';
+$string['description'] = 'Description';
+$string['price'] = 'Price';
+$string['stock'] = 'Stock';
+$string['unlimited'] = 'Unlimited';
+$string['category'] = 'Category';
+$string['course'] = 'Course';
+$string['attachment'] = 'Attachment';
+$string['images'] = 'Images';
+$string['actions'] = 'Actions';
+
+$string['marketplaceorders'] = 'Evoke Marketplace orders';
+$string['orders'] = 'Orders';
+$string['courseorders'] = 'Course orders';
+
+$string['categories'] = 'Categories';
+$string['category_create_button'] = 'Add a new category';
+
+$string['products'] = 'Products';
+$string['product_create_button'] = 'Add a new product';
+$string['product_type'] = 'Type';
+$string['type_digital'] = 'Digital';
+$string['type_digital_file'] = 'Digital downloadable file';
+$string['type_physical'] = 'Physical';
+
+$string['confirm_yes'] = 'Yes, I wanna it!';
+$string['confirm_no'] = 'Cancel';
+
+$string['createitem_success'] = 'Item successfuly created';
+$string['updateitem_success'] = 'Item successfuly updated';
+$string['deleteitem_success'] = 'Item successfuly deleted';
+
+$string['deleteitem_confirm_title'] = 'Are you sure?';
+$string['deleteitem_confirm_msg'] = 'Once deleted, this item cannot be recovered!';
+$string['deleteitem_itemwithchildren'] = 'You can not delete this item because it has some children items.';
+
+$string['buy'] = 'Buy';
+$string['buy_confirm_title'] = 'Confirm your purchase?';
+$string['buy_confirm_msg'] = 'Are you sure you want to buy this amazing item?';
+$string['buy_success'] = 'Congratulations! You just purchased an awesome item!';
+
+// Errors and validation message.
+$string['something_went_wrong'] = 'Something went wrong';
+$string['onlynumbers'] = 'Only numbers are accepted here';
+$string['onlyintegers'] = 'Only integer numbers are accepted here';
+$string['nonnegativevalues'] = 'You can not set negative values';
+
+$string['nocoinstobuy'] = 'Sorry. You don\'t have enough EVOCoins to buy this item.';
+$string['errorbuy'] = 'Something went wrong while trying to finish your purchase. Please, try again.';
+
+// Exceptions strings.
+$string['outofstock'] = 'Out of stock';
+$string['product_outofstock'] = 'This product is out of stock!';
+$string['exclusivecourseproduct'] = 'Exclusive Course Product';
+
+$string['mypurchases'] = 'My purchased products';
+$string['downloadyourfile'] = 'Download your file';
