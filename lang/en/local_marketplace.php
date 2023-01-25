2c83ef8af141d54479a484ae8c1f52165309f714
John Moravec <51122406+educationfutures@users.noreply.github.com>
2023-01-24 19:07:55 -0600

    fixed lang

--- lang/en/local_marketplace.php
+++ lang/en/local_marketplace.php
@@ -44,7 +44,7 @@ $string['type_digital'] = 'Digital';
 $string['type_digital_file'] = 'Digital downloadable file';
 $string['type_physical'] = 'Physical';
 
-$string['confirm_yes'] = 'Yes, I wanna it!';
+$string['confirm_yes'] = 'Yes, I want it!';
 $string['confirm_no'] = 'Cancel';
 
 $string['createitem_success'] = 'Item successfuly created';
@@ -53,25 +53,25 @@ $string['deleteitem_success'] = 'Item successfuly deleted';
 
 $string['deleteitem_confirm_title'] = 'Are you sure?';
 $string['deleteitem_confirm_msg'] = 'Once deleted, this item cannot be recovered!';
-$string['deleteitem_itemwithchildren'] = 'You can not delete this item because it has some children items.';
+$string['deleteitem_itemwithchildren'] = 'You cannot delete this item because it has child items.';
 
 $string['buy'] = 'Buy';
-$string['buy_confirm_title'] = 'Confirm your purchase?';
-$string['buy_confirm_msg'] = 'Are you sure you want to buy this amazing item?';
-$string['buy_success'] = 'Congratulations! You just purchased an awesome item!';
+$string['buy_confirm_title'] = 'Confirm your purchase';
+$string['buy_confirm_msg'] = 'Are you sure you want to buy this  item?';
+$string['buy_success'] = 'Congratulations! The item was purchased!';
 
 // Errors and validation message.
 $string['something_went_wrong'] = 'Something went wrong';
-$string['onlynumbers'] = 'Only numbers are accepted here';
-$string['onlyintegers'] = 'Only integer numbers are accepted here';
+$string['onlynumbers'] = 'Only numbers are allowed';
+$string['onlyintegers'] = 'Only integer numbers are allowed';
 $string['nonnegativevalues'] = 'You can not set negative values';
 
-$string['nocoinstobuy'] = 'Sorry. You don\'t have enough EVOCoins to buy this item.';
-$string['errorbuy'] = 'Something went wrong while trying to finish your purchase. Please, try again.';
+$string['nocoinstobuy'] = 'Sorry. You do not have enough Evocoins to buy this item.';
+$string['errorbuy'] = 'Something went wrong while trying to finish your purchase. Please try again.';
 
 // Exceptions strings.
 $string['outofstock'] = 'Out of stock';
-$string['product_outofstock'] = 'This product is out of stock!';
+$string['product_outofstock'] = 'Sorry. This product is out of stock.';
 $string['exclusivecourseproduct'] = 'Exclusive Course Product';
 
 $string['mypurchases'] = 'My purchased products';
