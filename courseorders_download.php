4c137d962fb234d8ce949dfbc5b4cc08fc75016a
Willian Mano <willianmanoaraujo@gmail.com>
2023-02-24 09:39:32 -0300

    Implement excel download of course report orders

--- courseorders_download.php
+++ courseorders_download.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Index admin page.
+ * Download course orders report
  *
  * @package     local_marketplace
  * @copyright   2022 World Bank Group <https://worldbank.org>
@@ -10,9 +10,11 @@
 
 require(__DIR__.'/../../config.php');
 
-global $DB, $CFG;
+global $DB;
 
 $id = required_param('id', PARAM_INT);
+$format = optional_param('format', 'csv', PARAM_ALPHA);
+
 $course = $DB->get_record('course', ['id' => $id], '*', MUST_EXIST);
 
 $context = context_course::instance($id);
@@ -20,14 +22,6 @@ $context = context_course::instance($id);
 require_course_login($id);
 
 require_capability('moodle/course:update', $context);
-
-require_once($CFG->libdir . '/csvlib.class.php');
-
-$csvexport = new \csv_export_writer('semicolon');
-
-$filename = strtolower(str_replace(' ', '_', trim($course->shortname))) . '_csv';
-
-$csvexport->set_filename(clean_filename($filename));
 
 $orderentity = new \local_marketplace\local\entities\order();
 $productentity = new \local_marketplace\local\entities\product();
@@ -38,7 +32,7 @@ if (!$orders) {
     throw new \Exception('No orders found');
 }
 
-$fieldnames = [
+$headers = [
     'id' => 'ID',
     'name' => 'Product name',
     'description' => 'Product description',
@@ -49,12 +43,11 @@ $fieldnames = [
     'usergroups' => 'User groups',
 ];
 
-// Add the header line to the data.
-$csvexport->add_data($fieldnames);
-
 $userutil = new \local_marketplace\util\user();
+
+$datatoexport = [];
 foreach ($orders as $order) {
-    $csvexport->add_data([
+    $datatoexport[] = [
         'id' => $order->id,
         'name' => $order->name,
         'description' => $order->description,
@@ -63,7 +56,63 @@ foreach ($orders as $order) {
         'fullname' => fullname($order),
         'email' => $order->email,
         'usergroups' => $userutil->get_user_groups_names($course->id, $order->userid)
-    ]);
+    ];
 }
 
-$csvexport->download_file();
+$filename = strtolower(str_replace(' ', '_', trim($course->shortname)));
+
+if (!$format || $format == 'csv') {
+    generate_csv_file($datatoexport, $filename, $headers);
+}
+
+if ($format == 'excel') {
+    generate_excel_file($datatoexport, $filename, $headers);
+}
+
+function generate_csv_file($datatoexport, $filename, $headers) {
+    global $CFG;
+
+    require_once($CFG->libdir . '/csvlib.class.php');
+
+    $csvexport = new \csv_export_writer('semicolon');
+
+    $csvexport->set_filename($filename);
+
+    $csvexport->add_data($headers);
+
+    foreach($datatoexport as $item) {
+        $csvexport->add_data($item);
+    }
+
+    $csvexport->download_file();
+}
+
+function generate_excel_file($datatoexport, $filename, $headers) {
+    global $CFG;
+
+    require_once("$CFG->libdir/excellib.class.php");
+
+    $filename = clean_filename($filename);
+
+    $workbook = new \MoodleExcelWorkbook($filename);
+
+    $worksheet[] = $workbook->add_worksheet(get_string('courseorders', 'local_marketplace'));
+
+    array_unshift($datatoexport, $headers);
+
+    $rowno = 0;
+    foreach($datatoexport as $row) {
+        $colno = 0;
+
+        foreach($row as $col) {
+            $worksheet[0]->write($rowno, $colno, $col);
+            $colno++;
+        }
+
+        $rowno++;
+    }
+
+    $workbook->close();
+
+    return $filename;
+}
