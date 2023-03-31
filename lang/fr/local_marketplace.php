868ff2b44018458e5cf055fb88ecb4414daa1503
Willian Mano <willian.mano@jove.com>
2023-03-31 15:11:53 -0300

    Add fr language

---
+++ lang/fr/local_marketplace.php
@@ -0,0 +1,88 @@
+<?php
+
+/**
+ * Lang file.
+ *
+ * @package     local_evokegame
+ * @copyright   2021 World Bank Group <https://worldbank.org>
+ * @author      Willian Mano <willianmanoaraujo@gmail.com>
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Place de Marché Evoke';
+$string['previous'] = 'Précédent';
+$string['next'] = 'Suivant';
+
+$string['marketplace'] = 'Place de Marché';
+$string['home'] = 'Accueil';
+
+// Form elements.
+$string['selectanoption'] = 'Sélectionnez une option';
+$string['name'] = 'Nom';
+$string['description'] = 'Description';
+$string['price'] = 'Prix';
+$string['stock'] = 'Stock';
+$string['limitperuser'] = 'Limite par utilisateur';
+$string['limitperuser_help'] = 'Ce champ définit combien de fois un utilisateur peut acheter ce produit. Laissez-le vide à illimité.';
+$string['unlimited'] = 'Illimité';
+$string['category'] = 'Catégorie';
+$string['course'] = 'Cours';
+$string['attachment'] = 'Pièce-jointe';
+$string['images'] = 'Images';
+$string['actions'] = 'Actions';
+
+$string['marketplaceorders'] = 'Évoquer les commandes de la place de marché';
+$string['orders'] = 'Commandes';
+$string['courseorders'] = 'Ordres de cours';
+$string['courseproducts'] = 'Produits du cours';
+
+$string['categories'] = 'Catégories';
+$string['category_create_button'] = 'Ajouter une nouvelle catégorie';
+
+$string['products'] = 'Produits';
+$string['product_create_button'] = 'Ajouter un nouveau produit';
+$string['product_type'] = 'Type';
+$string['type_digital'] = 'Digital';
+$string['type_digital_file'] = 'Fichier numérique téléchargeable';
+$string['type_physical'] = 'Physique';
+
+$string['confirm_yes'] = 'Oui, je le veux !';
+$string['confirm_no'] = 'Annuler';
+
+$string['createitem_success'] = 'Élément créé avec succès';
+$string['updateitem_success'] = 'Article mis à jour avec succès';
+$string['deleteitem_success'] = 'Élément supprimé avec succès';
+
+$string['deleteitem_confirm_title'] = 'Êtes-vous sûr ?';
+$string['deleteitem_confirm_msg'] = 'Une fois supprimé, cet élément ne peut pas être récupéré !';
+$string['deleteitem_itemwithchildren'] = 'Vous ne pouvez pas supprimer cet élément car il a des éléments enfants.';
+
+$string['buy'] = 'Buy';
+$string['buy_confirm_title'] = 'Confirmez votre achat';
+$string['buy_confirm_msg'] = 'Êtes-vous sûr de vouloir acheter cet article ?';
+$string['buy_success'] = 'Félicitations! L\'article a été acheté.';
+
+// Errors and validation message.
+$string['Something_went_wrong'] = 'Quelque chose s\'est mal passé';
+$string['onlynumbers'] = 'Seuls les nombres sont autorisés';
+$string['onlyintegers'] = 'Seuls les nombres entiers sont autorisés';
+$string['nonnegativevalues'] = 'Vous ne pouvez pas définir de valeurs négatives';
+
+$string['nocoinstobuy'] = 'Désolé. Vous n\'avez pas assez d\'EVC pour acheter cet article.';
+$string['errorbuy'] = 'Une erreur s\'est produite lors de la tentative de finalisation de votre achat. Veuillez réessayer.';
+
+// Exceptions strings.
+$string['outofstock'] = 'Rupture de stock';
+$string['product_outofstock'] = 'Désolé. Ce produit est en rupture de stock.';
+$string['exclusivecourseproduct'] = 'Produit de cours exclusif';
+$string['order_limitperuser'] = 'Vous atteignez la limite d\'achats pour ce produit';
+
+$string['mypurchases'] = 'Mes produits achetés';
+$string['downloadyourfile'] = 'Télécharger votre fichier';
+
+$string['downloadordersreport'] = 'Télécharger le rapport des commandes';
+$string['coursenoorders'] = 'Ce cours n\'a pas encore de commandes';
+
+$string['total'] = 'Total';
+$string['months'] = 'Janvier,Février,Mars,Avril,Mai,Juin,Juillet,Août,Septembre,Octobre,Novembre,Décembre';
